125891e3564923524992a78c701262e4aedeeaaa
gnome_x11_sddm_arch <nz@gmail.com>
2025-02-20 12:04:24 +0700

    v1

--- src/layouts/default_layout.jsx
+++ src/layouts/default_layout.jsx
@@ -1,28 +1,21 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Box, Heading, Text, List, ListItem } from "@chakra-ui/react";
 export default function Default_Layout() {
+  const md = `---
+<www.nisith.com>
+# Hello
+**Vite!**\n\n- This is a list\n- With some items\n\n
+---
+[goto](wwwm)
+> hekkiw`;
   return (
     <>
       <div className=" w-full h-full flex flex-col justify-center items-center">
         <h1>wellcome to my template </h1>
-
-        <section>
-          <h2 className="text-3xl font-semibold text-gray-800 mb-6">
-            Features
-          </h2>
-          <ul className="list-disc pl-5 space-y-4 text-lg">
-            <li>Chakra UI for flexible and customizable UI components.</li>
-            <li>TailwindCSS for utility-first CSS styling.</li>
-            <li>
-              React and React Router for building interactive UIs and handling
-              client-side routing.
-            </li>
-            <li>
-              Next-Themes for easy theme switching between light and dark modes.
-            </li>
-            <li>Axios for making HTTP requests.</li>
-            <li>ESLint setup for better code quality and consistency.</li>
-            <li>clsx utility for conditional class names.</li>
-          </ul>
-        </section>
+        <div id="md" className=" w-full h-full">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{md}</ReactMarkdown>
+        </div>
       </div>
     </>
   );
